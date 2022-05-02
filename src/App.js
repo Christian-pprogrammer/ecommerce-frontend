4632044d0cb151dd9634323d69588c2d46b41409
Christian-pprogrammer <mpanoc6@gmail.com>
2022-05-02 12:52:39 +0200

    change app name

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
           <Navbar bg='dark' variant='dark'>
             <Container>
               <LinkContainer to='/'>
-                <Navbar.Brand>Amazona</Navbar.Brand>
+                <Navbar.Brand>E-soko</Navbar.Brand>
               </LinkContainer>
               <Nav className='me-auto'>
                 <Link to='/cart' className='nav-link'>
